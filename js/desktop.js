a8c7c37703bb78f089d16d0126a801b1c6435d98
Werik Jahel <werikjahel@gmail.com>
2023-11-15 17:46:44 -0300

    Correção: best-sellers e outros;

--- js/desktop.js
+++ js/desktop.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "Deskop",
     "Intel Core i5",
-    "1239,99",
+    "1,239.99",
     "https://m.media-amazon.com/images/I/61h-rFM1QdL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/Computador-Completo-Desktop-Monitor-3D-051/dp/B0BVJGKCJB/ref=sr_1_3_sspa?c=ts&keywords=CPUs+de+Computadores&qid=1697584304&s=computers&sr=1-3-spons&ts_id=16364819011&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
   );
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "16 GB",
     "Office",
     "Intel Core i7",
-    "1649,99",
+    "1,649.99",
     "https://m.media-amazon.com/images/I/51vmA52feFL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/Computador-Completo-Windows-Monitor-3green/dp/B0CFYMK52W/ref=sr_1_10?c=ts&keywords=CPUs+de+Computadores&qid=1697584304&s=computers&sr=1-10&ts_id=16364819011&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
   );
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "16 GB",
     "Computador",
     "Intel Core i7-2600",
-    "1599,01",
+    "1,599.01",
     "https://m.media-amazon.com/images/I/716DuGwWLAL._AC_SL1200_.jpg",
     "https://www.amazon.com.br/Computador-Intel-N%C3%BAcleos-Strong-Tech/dp/B0BS1QT16H/ref=sr_1_9?c=ts&keywords=CPUs+de+Computadores&qid=1697589446&s=computers&sr=1-9&ts_id=16364819011"
   );
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "Office",
     "Intel Core i5",
-    "1066,41",
+    "1,066.41",
     "https://m.media-amazon.com/images/I/51LnfULhGYL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/Computador-Flex-Computer-Monitor-Windows/dp/B09BD5PTWW/ref=sr_1_10?c=ts&keywords=CPUs%2Bde%2BComputadores&qid=1697589446&s=computers&sr=1-10&ts_id=16364819011&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&th=1"
   );
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "49147",
     "Intel Core i5",
-    "1369,99",
+    "1,369.99",
     "https://m.media-amazon.com/images/I/61xm1msZ6NL._AC_SL1200_.jpg",
     "https://www.amazon.com.br/Computador-Completo-Windows-Monitor-ElitePC/dp/B0977T1YC2/ref=sr_1_48?c=ts&keywords=CPUs+de+Computadores&qid=1697589862&s=computers&sr=1-48&ts_id=16364819011&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
   );
@@ -126,7 +126,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tagDiv.appendChild(tagP);
 
     var tagPreco = document.createElement("p");
-    tagPreco.textContent = `R$ ${preco}`;
+    tagPreco.textContent = `$ ${preco}`;
 
     tagDiv.appendChild(tagPreco);
 
--- js/laptops.js
+++ js/laptops.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "SSD",
     "Macbook Air",
     "Apple",
-    "7899,98",
+    "7,899.98",
     "https://m.media-amazon.com/images/I/41j6lmxrbPL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/MacBook-Air-13-polegadas-Processador-Apple-GPU-7%E2%80%91core/dp/B09L528HM1/ref=sr_1_1_sspa?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=DHI5GNHSM4PR&keywords=laptop&qid=1697667406&sprefix=laptops%2Caps%2C849&sr=8-1-spons&ufe=app_do%3Aamzn1.fos.95de73c3-5dda-43a7-bd1f-63af03b14751&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
   );
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "Macbook Air",
     "Others",
-    "13499,00",
+    "13,499.00",
     "https://m.media-amazon.com/images/I/51ppYyWOrNL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/Notebook-Apple-MacBook-Air-13-polegadas/dp/B0B3CQVB62/ref=sr_1_2_sspa?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=DHI5GNHSM4PR&keywords=laptop&qid=1697667406&sprefix=laptops%2Caps%2C849&sr=8-2-spons&ufe=app_do%3Aamzn1.fos.95de73c3-5dda-43a7-bd1f-63af03b14751&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
   );
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "Chromebook",
     "Intel Core i7-2600",
-    "1599,01",
+    "1,599.01",
     "https://m.media-amazon.com/images/I/71Kw0eLj+aL._AC_SL1500_.jpg",
     "https://www.amazon.com.br/Computador-Intel-N%C3%BAcleos-Strong-Tech/dp/B0BS1QT16H/ref=sr_1_9?c=ts&keywords=CPUs+de+Computadores&qid=1697589446&s=computers&sr=1-9&ts_id=16364819011"
   );
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "4 GB",
     "IdeaPad 3i",
     "Intel Core i3-1115G4",
-    "1979,10",
+    "1,979.10",
     "https://m.media-amazon.com/images/I/61PrCgQ2KwL._AC_SL1500_.jpg",
     "https://www.amazon.com.br/Notebook-Lenovo-i3-1115G4-Graphics-82MD000ABR/dp/B09ZKCVTFR/ref=sr_1_13?crid=1MKQJ8XPPY711&keywords=laptop&qid=1700077576&sprefix=%2Caps%2C201&sr=8-13&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
   );
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "8 GB",
     "49147",
     "Intel Core i5 11th",
-    "3199,00",
+    "3,199.00",
     "https://m.media-amazon.com/images/I/51EgiK+12uL._AC_SL1000_.jpg",
     "https://www.amazon.com.br/Notebook-VAIO-Intel%C2%AE-Antirreflexo-Windows/dp/B0CB4V3L1B/ref=sr_1_7?crid=1MKQJ8XPPY711&keywords=laptop&qid=1700077576&sprefix=%2Caps%2C201&sr=8-7&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
   );
@@ -129,7 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tagDiv.appendChild(tagP);
 
     var tagPreco = document.createElement("p");
-    tagPreco.textContent = `R$ ${preco}`;
+    tagPreco.textContent = `$ ${preco}`;
 
     tagDiv.appendChild(tagPreco);
 
